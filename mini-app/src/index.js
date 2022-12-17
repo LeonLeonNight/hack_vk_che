d2c45ecaa9f9691d33f2a1f50cc47c5a29767716
Nikolay <soloviev_nikolay@mail.ru>
2022-12-17 21:23:46 +0500

    added tunnel vk (for using, run "npm run tunnel")

--- mini-app/src/index.js
+++ mini-app/src/index.js
@@ -4,6 +4,7 @@ import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
 // Init VK  Mini App
+// Отправляет событие инициализации нативному клиенту
 bridge.send("VKWebAppInit");
 
 ReactDOM.render(<App />, document.getElementById("root"));
